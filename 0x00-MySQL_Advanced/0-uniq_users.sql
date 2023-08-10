535bed0b0676e25e32b6e3d2cc9d83e7c70c2050
Hope Davids <hopeolawole@gmail.com>
2023-08-10 22:06:28 +0000

    modified - This SQL script that creates a table users and atrributes

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,8 +1,8 @@
 -- An SQL script that creates a table users following these requirements:
 -- Make an attribute unique directly in the table schema will enforced your business rules and avoid bugs in your application
 
-CREATE TABLE IF NOT EXISTS "users" (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS users (
+    id SERIAL PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
     name VARCHAR(255)
 );
