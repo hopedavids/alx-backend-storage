19f6640b15789406ed911ba84e7272e1d713e48d
Hope Davids <hopeolawole@gmail.com>
2023-08-16 22:17:56 +0000

    Implementing an expiring web cache and tracker

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,36 +1,44 @@
 #!/usr/bin/env python3
 
+""" Implementing an expiring web cache and tracker """
+
+
 import requests
 import time
 import redis
 
+
 class WebCache:
     def __init__(self, host='localhost', port=6379):
         self._redis = redis.Redis(host=host, port=port, decode_responses=True)
-    
+
     def get_page(self, url: str) -> str:
-        # Check if the URL is already cached
+        """ Check if the URL is already cached.Implementing
+            an expiring web cache and tracker.
+        """
+
         cached_content = self._redis.get(url)
         if cached_content:
             print(f"Cache hit for URL: {url}")
             return cached_content
-        
+
         # Fetch the page content
         response = requests.get(url)
         page_content = response.text
-        
+
         # Cache the content with a 10-second expiration
         self._redis.setex(url, 10, page_content)
-        
+
         # Track URL access count
         self._redis.incr(f"count:{url}")
-        
+
         return page_content
 
     def get_access_count(self, url: str) -> int:
         # Get the access count for the URL
         count = self._redis.get(f"count:{url}")
         return int(count) if count else 0
+
 
 # Usage example
 web_cache = WebCache()
